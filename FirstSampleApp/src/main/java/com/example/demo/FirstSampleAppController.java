177b82114a39ca92dd0d5e85c1173027f46aeafd
vsmk-common-git <162783897+vsmk-common-git@users.noreply.github.com>
2024-03-09 15:47:48 +0530

    FirstCommit_in Main branch_Sample API

---
+++ FirstSampleApp/src/main/java/com/example/demo/FirstSampleAppController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class FirstSampleAppController {
+
+	@RequestMapping("/Hello")
+	public String sayHello() {
+		return "hello!";
+	}
+	
+	
+}
